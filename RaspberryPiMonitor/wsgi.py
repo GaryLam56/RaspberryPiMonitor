d19344cc1c4cd442044960028160b7c800aa8e13
Gary Lam <garylam5678@gmail.com>
2016-08-04 00:34:31 -0400

    fixing runtime error

--- RaspberryPiMonitor/wsgi.py
+++ RaspberryPiMonitor/wsgi.py
@@ -3,8 +3,18 @@ import sys
 import site
 from django.core.wsgi import get_wsgi_application
 
-def application(environ, start_response):
-    if environ['mod_wsgi.process_group'] != '':
-        import signal
-        os.kill(os.getpid(), signal.SIGINT)
-    return ["killed"]
+# Add the site-packages of the chosen virtualenv to work with
+site.addsitedir('/home/pi/py3env/lib/python3.4/site-packages')
+
+# Add the app's directory to the PYTHONPATH
+sys.path.append('/home/pi/RaspberryPiMonitor/')
+sys.path.append('/home/pi/RaspberryPiMonitor/RaspberryPiMonitor')
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'RaspberryPiMonitor.settings'
+
+# Activate your virtual env
+activate_env=os.path.expanduser("/home/pi/py3env/bin/activate_this.py")
+# execfile(activate_env, dict(__file__=activate_env))
+exec(compile(open(activate_env, "rb").read(), activate_env, 'exec'), dict(__file__=activate_env))
+
+application = get_wsgi_application()
