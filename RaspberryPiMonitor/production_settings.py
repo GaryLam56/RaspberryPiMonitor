41e7f3e012057f980c160bdcc8dd28971809c235
Gary Lam <garylam5678@gmail.com>
2016-08-04 16:56:39 -0400

    added an app to settings.py

--- RaspberryPiMonitor/production_settings.py
+++ RaspberryPiMonitor/production_settings.py
@@ -19,6 +19,7 @@ ALLOWED_HOSTS = ['.perfectkiwi.xyz']
 # Application definition
 
 INSTALLED_APPS = [
+    'monitor.apps.MonitorConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
