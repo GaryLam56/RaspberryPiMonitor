c87b054a1f634229d30ebd1862dfdae08eafbbf7
Gary Lam <garylam5678@gmail.com>
2016-08-03 22:45:38 -0400

    updated wsgi.py

--- RaspberryPiMonitor/wsgi.py
+++ RaspberryPiMonitor/wsgi.py
@@ -1,16 +1,23 @@
-"""
-WSGI config for RaspberryPiMonitor project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/dev/howto/deployment/wsgi/
-"""
-
 import os
+import sys
+import site
+
+# Add the site-packages of the chosen virtualenv to work with
+site.addsitedir('/home/pi/py3env/lib/python3.4/site-packages')
+
+# Add the app's directory to the PYTHONPATH
+sys.path.append('/home/pi/RaspberryPiMonitor/')
+sys.path.append('/home/pi/RaspberryPiMonitor/RaspberryPiMonitor')
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'RaspberryPiMonitor.production_settings'
+
+# Activate your virtual env
+activate_env=os.path.expanduser("/home/pi/py3env/bin/activate_this.py")
+# execfile(activate_env, dict(__file__=activate_env))
+exec(compile(open(activate_env, "rb").read(), activate_env, 'exec'), dict(__file__=activate_env))
+
+import django.core.handlers.wsgi
+application = django.core.handlers.wsgi.WSGIHandler()
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "RaspberryPiMonitor.settings")
-
 application = get_wsgi_application()
