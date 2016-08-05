dc907743c92570c6db612619e9ba4a27b6653337
Gary Lam <garylam5678@gmail.com>
2016-08-04 23:22:20 -0400

    removed user field from raspberry

--- monitor/models.py
+++ monitor/models.py
@@ -3,7 +3,6 @@ from django.contrib.auth.models import User
 
 
 class Raspberry(models.Model):
-    user = models.ForeignKey(User, default=1)
     temperature = models.DecimalField(decimal_places=2, max_digits=4)
     memory_used = models.IntegerField()
 
