a6bd2093c6b56a9fa3ae5ab8c416112479eb4b53
Gary Lam <garylam5678@gmail.com>
2016-08-05 10:32:16 -0400

    changed temperature fields

--- monitor/models.py
+++ monitor/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 
 class Raspberry(models.Model):
-    temperature = models.DecimalField(decimal_places=1, max_digits=3)
+    temperature = models.FloatField()
     memory_used = models.IntegerField()
 
     def __str__(self):
