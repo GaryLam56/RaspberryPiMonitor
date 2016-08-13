73ff0def4a2dc8250b9f2bacbf349a7078e4d355
Gary Lam <garylam5678@gmail.com>
2016-08-13 10:57:46 -0400

    updated ajax

--- monitor/views.py
+++ monitor/views.py
@@ -42,7 +42,7 @@ def test(request):
     if request.method == 'GET':
         response_data = {}
         response_data['temp'] = getTemperature()
-        response_data['mem_stats'] = int(getRamStats()[1][:-1])
+        response_data['mem_stats'] = int(getRamStats())
         return HttpResponse(json.dumps(response_data), content_type="application/json")
 
 
