34210419a20570a8621cf9eeb3e4d2c506b1b26e
Gary Lam <garylam5678@gmail.com>
2016-08-13 13:12:03 -0400

    update .js file

---
+++ monitor/static/monitor/js/myscript.js
@@ -0,0 +1,24 @@
+function update() {
+    $(document).ready(function () {
+        $("#btn").click(function () {
+            $.ajax({
+                type: "GET",
+                url: "/test/",
+                dataType: "json",
+                data: {csrfmiddlewaretoken: '{{ csrf_token }}'},
+                success: function (json) {
+                    var x = ((json.mem_used / json.mem_total) * 100);
+                    console.log(x);
+                    $("#memory").attr({
+                        "style": ("width:" + x + "%"),
+                        "aria-valuemax": (json.mem_total),
+                        "aria-valuenow": (json.mem_used)
+                    });
+                },
+                error: function () {
+                    alert("error!");
+                }
+            });
+        });
+    });
+}
