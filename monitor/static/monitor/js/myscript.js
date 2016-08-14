ab34e8ca2134767eb2cd47c979b15275f15200a1
Gary Lam <garylam5678@gmail.com>
2016-08-14 12:12:18 -0400

    updated myscript.js

--- monitor/static/monitor/js/myscript.js
+++ monitor/static/monitor/js/myscript.js
@@ -14,7 +14,7 @@ function update() {
                         "aria-valuemax": (json.mem_total),
                         "aria-valuenow": (json.mem_used)
                     });
-                    $("#pi-uptime").html(x + "%");
+                    $("#pi-uptime").html(json.up_day + "d" + json.up_hour + "h" + json.up_minute + "m");
                     $("#buffer-percentage").html(((json.mem_buffer/json.mem_total) * 100) + "%");
                     $("#cache-percentage").html(((json.mem_cache/json.mem_total) * 100) + "%");
                     $("#used-percentage").html(((json.mem_used/json.mem_total) * 100) + "%");
