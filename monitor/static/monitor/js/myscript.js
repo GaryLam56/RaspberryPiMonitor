46504bea093d55c357abb66f8ab9a2e3b0144fc6
Gary Lam <garylam5678@gmail.com>
2016-10-07 22:06:06 -0400

    updated javascript

--- monitor/static/monitor/js/myscript.js
+++ monitor/static/monitor/js/myscript.js
@@ -8,11 +8,10 @@ function update() {
                 data: {csrfmiddlewaretoken: '{{ csrf_token }}'},
                 success: function (json) {
                     var x = ((json.mem_used / json.mem_total) * 100);
-                    console.log(x);
                     $("#memory").attr({
                         "style": ("width:" + x + "%"),
                         "aria-valuemax": (json.mem_total),
-                        "aria-valuenow": (json.mem_used)
+                        "aria-valuenow": (json.mem_used),
                     });
                     $("#pi-uptime").html(json.up_time);
                     $("#buffer-percentage").html(json.mem_buffer + "MB");
